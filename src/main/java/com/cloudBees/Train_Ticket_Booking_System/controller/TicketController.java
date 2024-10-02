df5ceccd4c8e460f59df3a743a62a9bc745064e4
falgunbokde23 <falgunbokde@gmail.com>
2024-10-02 22:51:54 +0530

    javadoc added

--- src/main/java/com/cloudBees/Train_Ticket_Booking_System/controller/TicketController.java
+++ src/main/java/com/cloudBees/Train_Ticket_Booking_System/controller/TicketController.java
@@ -21,19 +21,24 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ticket")
+@Tag(name = "Ticket Controller", description = "API for managing Ticket (**Dev @Falgun**)")
 public class TicketController {
-
 
     @Autowired
     private TicketService ticketService;
 
+    /**
+     * Books a train ticket.
+     *
+     * @param ticketDto the details of the ticket to be booked
+     * @return a response entity containing the booking status and ticket details
+     */
     @Operation(summary = "Book a train ticket", description = "Allows a user to book a train ticket.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Ticket successfully booked",
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponse.class))),
             @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponse.class))
-    )
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomResponse.class)))
     })
     @PostMapping("/book")
     public ResponseEntity<CustomResponse> bookTicket(@Valid @RequestBody TicketDto ticketDto) {
@@ -42,14 +47,27 @@ public class TicketController {
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
-    @Operation(summary = "View receipt by user", description = "Retrieves receipt to view all the ticket details by by user email.")
+    /**
+     * Retrieves a receipt to view all ticket details by user email.
+     *
+     * @param email the email of the user whose ticket details are to be retrieved
+     * @return a response entity containing the ticket details or a not found status
+     */
+    @Operation(summary = "View receipt by user", description = "Retrieves receipt to view all the ticket details by user email.")
     @GetMapping("/receipt/{email}")
     public ResponseEntity<CustomResponse> getTicketByEmail(@PathVariable String email) {
         Ticket ticket = ticketService.getTicketByEmail(email);
-        CustomResponse response = new CustomResponse(ticket != null ? HttpStatus.OK.value() : HttpStatus.NOT_FOUND.value(), ticket != null ? Status.SUCCESS.name() : Status.FAILED.name(), ticket);
+        CustomResponse response = new CustomResponse(ticket != null ? HttpStatus.OK.value() : HttpStatus.NOT_FOUND.value(), 
+                ticket != null ? Status.SUCCESS.name() : Status.FAILED.name(), ticket);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    /**
+     * Retrieves a list of users seated in a specified section.
+     *
+     * @param section the section to check for seated users
+     * @return a response entity containing the list of users in the specified section
+     */
     @Operation(summary = "View Users with seat by Section", description = "Retrieves a list of users seated in a specified section.")
     @GetMapping("/section/{section}")
     public ResponseEntity<CustomResponse> getUsersBySection(@PathVariable String section) {
@@ -58,7 +76,13 @@ public class TicketController {
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
-    @Operation(summary = "Remove user from train", description = " Remove specific user from the train.")
+    /**
+     * Removes a specific user from the train.
+     *
+     * @param email the email of the user to be removed
+     * @return a response entity indicating the result of the operation
+     */
+    @Operation(summary = "Remove user from train", description = "Remove specific user from the train.")
     @DeleteMapping("/remove/{email}")
     public ResponseEntity<CustomResponse> removeUser(@PathVariable String email) {
         ticketService.removeUser(email);
@@ -66,15 +90,27 @@ public class TicketController {
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    /**
+     * Modifies a user's train seat.
+     *
+     * @param email   the email of the user whose seat is to be modified
+     * @param newSeat the new seat to assign to the user
+     * @return a response entity containing the updated ticket details or a not found status
+     */
     @Operation(summary = "Modify a train seat", description = "Allows a user to modify a train seat.")
     @PutMapping("/modify-seat/{email}")
     public ResponseEntity<CustomResponse> modifyUserSeat(@PathVariable String email, @RequestParam String newSeat) {
         Ticket updatedTicket = ticketService.modifyUserSeat(email, newSeat);
-        CustomResponse response = new CustomResponse(updatedTicket != null  ? HttpStatus.OK.value() : HttpStatus.NOT_FOUND.value(),
+        CustomResponse response = new CustomResponse(updatedTicket != null ? HttpStatus.OK.value() : HttpStatus.NOT_FOUND.value(),
                 updatedTicket != null ? Status.SUCCESS.name() : Status.FAILED.name(), updatedTicket);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    /**
+     * Checks available train seats.
+     *
+     * @return a response entity containing a list of available seats
+     */
     @Operation(summary = "Check available train seats", description = "Allows a user to check available train seats.")
     @GetMapping("/available-seats")
     public ResponseEntity<CustomResponse> getAvailableSeats() {
